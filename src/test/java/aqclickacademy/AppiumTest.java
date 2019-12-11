cd3ed0c8b547c12aa8472b12844c5da5fd79f784
felix0010 <scion98@gmail.com>
2019-12-10 17:45:27 -0800

    first commit

---
+++ src/test/java/aqclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package aqclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPpAndroid() 
+	{
+	System.out.println("NativeAPpAndroid");	
+	}
+	@Test 
+	public void IOSApps() 
+	{
+	System.out.println("ISOApps");
+	}
+
+}
